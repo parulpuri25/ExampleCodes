bc3fc00aa7037832aee92373cdc4ac356e235a2f
parulpuri25 <93627829+parulpuri25@users.noreply.github.com>
2023-07-03 15:54:22 -0400

    Create minValue.py

---
+++ minValue.py
@@ -0,0 +1,7 @@
+#Find Minimum Value in list
+def findMin(self, nums: List[int]) -> int:
+        min_val = nums[0]
+        for i in range(1,len(nums)):
+            if min_val > nums[i]:
+                min_val = nums[i]
+        return min_val
