20bae2b2adcaa714d93e91590c6425a88f5fd7a4
Parul Puri <93627829+parulpuri25@users.noreply.github.com>
2023-08-23 15:55:13 -0400

    Create HappyNumber.py

---
+++ HappyNumber.py
@@ -0,0 +1,10 @@
+def isHappy(n):
+    seen = set()
+    while(n!=1):
+        n = sum([int(i)**2 for i in str(n)])
+        if n in seen:
+            return False
+        else:
+            seen.add(n)
+    else:
+        return True
